376e4af250d5545ab61b8a6ba3b844c66292a34e
Svetlin Ralchev <iamralch@users.noreply.github.com>
2024-08-22 13:21:11 +0400

    Update the examples
    Signed-off-by: Svetlin Ralchev <iamralch@users.noreply.github.com>

--- tracer_test.go
+++ tracer_test.go
@@ -2,13 +2,13 @@ package pgxotel_test
 
 import (
 	"context"
+	"fmt"
 	"os"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/pgx-contrib/pgxotel"
 )
-
-var count int
 
 func ExampleTracer() {
 	config, err := pgxpool.ParseConfig(os.Getenv("PGX_DATABASE_URL"))
@@ -22,9 +22,28 @@ func ExampleTracer() {
 	if err != nil {
 		panic(err)
 	}
+	// close the connection
+	defer conn.Close()
 
-	row := conn.QueryRow(context.TODO(), "SELECT 1")
-	if err := row.Scan(&count); err != nil {
+	rows, err := conn.Query(context.TODO(), "SELECT * from customer")
+	if err != nil {
 		panic(err)
+	}
+	// close the rows
+	defer rows.Close()
+
+	// Customer struct must be defined
+	type Customer struct {
+		FirstName string `db:"first_name"`
+		LastName  string `db:"last_name"`
+	}
+
+	for rows.Next() {
+		customer, err := pgx.RowToStructByName[Customer](rows)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(customer.FirstName)
 	}
 }
